58ddf19f4a9ea19a67942b030bd130fffab25c6b
Dominika Bobik <dominikabobik12@gmail.com>
2023-06-07 21:43:15 -0700

    print borad

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn print_board(array: [[char; 3]; 3])
 {
-    println!("{}", format!("{} | {} | {}", array[0][0], array[0][1], array[0][2]));
-    println!("--+---+--");
-    println!("{}", format!("{} | {} | {}", array[1][0], array[1][1], array[1][2]));
-    println!("--+---+--");
-    println!("{}", format!("{} | {} | {}", array[2][0], array[2][1], array[2][2]));
+    println!("{}", format!(" {} | {} | {}", array[0][0], array[0][1], array[0][2]));
+    println!("---+---+---");
+    println!("{}", format!(" {} | {} | {}", array[1][0], array[1][1], array[1][2]));
+    println!("---+---+---");
+    println!("{}", format!(" {} | {} | {}", array[2][0], array[2][1], array[2][2]));
 }
 
 fn main() {
